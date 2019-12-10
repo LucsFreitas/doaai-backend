db2908b10e53ad03c592e004b2010bbd75bab454
Lucas Freitas <lucas_vbf@hotmail.com>
2019-12-09 21:08:56 -0300

    Inicialização do projeto

---
+++ src/main/java/br/unicap/doaai/doaai/DoaaiApplication.java
@@ -0,0 +1,13 @@
+package br.unicap.doaai.doaai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoaaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoaaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/unicap/doaai/doaai/DoaaiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.unicap.doaai.doaai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoaaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
